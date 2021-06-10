a1f18e14b299d6af55ed0d2bcac6a77814db8f1f
radoneje <den.shevchenko@gmail.com>
2021-06-10 19:45:14 +0300

    cc

--- public/javascripts/mainScript.js
+++ public/javascripts/mainScript.js
@@ -1,271 +1,292 @@
-var app=new Vue({
-    el:"#app",
-    data:{
-        pgmItemModal:null,
-        spkItemModal:null,
-        reqModal:null,
-        user:JSON.parse( localStorage.getItem("user") || '{"isPromice":false}'),
-        reqProcess:false,
-        tracks:[],
-        tracksDate:[],
-        currTrack:{},
-        currShowTrack:{},
-        trackRotateTimeout:0,
-        pgm:[],
-        spk:[],
-        curSpeakersPage:0,
-        curSpeakers:[],
-        faq:[],
-        menuModal:false,
-    },
-    methods:{
-        getSessionFromSpk:function(spk){
-            var ret=[];
+var app;
 
-            this.pgm.forEach(p=>{
-                if(p.moderators)
-                    p.moderators.forEach(m=>{
-                        if(m==spk.id)
-                            ret.push(p);
+        app = new Vue({
+            el: "#app",
+            data: {
+                pgmItemModal: null,
+                spkItemModal: null,
+                reqModal: null,
+                user: JSON.parse(localStorage.getItem("user") || '{"isPromice":false}'),
+                reqProcess: false,
+                tracks: [],
+                tracksDate: [],
+                currTrack: {},
+                currShowTrack: {},
+                trackRotateTimeout: 0,
+                pgm: [],
+                spk: [],
+                curSpeakersPage: 0,
+                curSpeakers: [],
+                faq: [],
+                menuModal: false,
+            },
+            methods: {
+                getSessionFromSpk: function (spk) {
+                    var ret = [];
+
+                    this.pgm.forEach(p => {
+                        if (p.moderators)
+                            p.moderators.forEach(m => {
+                                if (m == spk.id)
+                                    ret.push(p);
+                            })
+                        if (p.speakers)
+                            p.speakers.forEach(m => {
+                                if (m == spk.id)
+                                    ret.push(p);
+                            })
+
                     })
-                if(p.speakers)
-                    p.speakers.forEach(m=>{
-                        if(m==spk.id)
-                            ret.push(p);
-                    })
 
-            })
-
-            ret.forEach(p=>{
-             //   var track=this.tracks.filter(t=>t.id==p.trackid);
-             //   p.key=moment(track[0].data).format("DD MMM")
-            });
+                    ret.forEach(p => {
+                        //   var track=this.tracks.filter(t=>t.id==p.trackid);
+                        //   p.key=moment(track[0].data).format("DD MMM")
+                    });
 
 
-           return ret;
-        },
-        curSpeakersPagePreview:function(){
-            this.curSpeakersPage--;
-            if(this.curSpeakersPage<0)
-                this.curSpeakersPage=0;
-        },
-        curSpeakersPageNext:function(){
-            this.curSpeakersPage++;
-            if(this.curSpeakersPage>parseInt(this.spk.length/8))
-                this.curSpeakersPage--;
-        },
-        getHtml:function(string){
-            if(!string)
-                return "<br/>";
-            string=string.replace(/\n/g,"<br/>");
-            return  string
-        },
+                    return ret;
+                },
+                curSpeakersPagePreview: function () {
+                    this.curSpeakersPage--;
+                    if (this.curSpeakersPage < 0)
+                        this.curSpeakersPage = 0;
+                },
+                curSpeakersPageNext: function () {
+                    this.curSpeakersPage++;
+                    if (this.curSpeakersPage > parseInt(this.spk.length / 8))
+                        this.curSpeakersPage--;
+                },
+                getHtml: function (string) {
+                    if (!string)
+                        return "<br/>";
+                    string = string.replace(/\n/g, "<br/>");
+                    return string
+                },
 
-        regUser:async function(){
-            if(this.reqProcess)
-                return ;
-            if(!this.checkUser())
-                return ;
-            if(this.user.id)
-                return ;
-            this.reqProcess=true;
-            var ret=await axios.post("/api/regUser/"+lang, this.user);
-            this.user=ret.data;
-            this.user.isPromice=true;
-            localStorage.setItem("user", JSON.stringify(this.user))
-            this.reqProcess=false;
-        },
-        checkUser:function() {
+                regUser: async function () {
+                    if (this.reqProcess)
+                        return;
+                    if (!this.checkUser())
+                        return;
+                    if (this.user.id)
+                        return;
+                    this.reqProcess = true;
+                    var ret = await axios.post("/api/regUser/" + lang, this.user);
+                    this.user = ret.data;
+                    this.user.isPromice = true;
+                    localStorage.setItem("user", JSON.stringify(this.user))
+                    this.reqProcess = false;
+                },
+                checkUser: function () {
 
-            if(!this.user.f)
-               return false
-            if(!this.user.i)
-                return false
-            if(!this.user.email)
-                return false
-            if(!this.user.isPromice)
-                return false
+                    if (!this.user.f)
+                        return false
+                    if (!this.user.i)
+                        return false
+                    if (!this.user.email)
+                        return false
+                    if (!this.user.isPromice)
+                        return false
 
-            if(this.user.f.length<2)
-                return false
-            if(this.user.i.length<2)
-                return false
-            console.log("validateEmail", validateEmail(this.user.email))
-            if(!validateEmail(this.user.email))
-                return false
-
-
-            return true;
+                    if (this.user.f.length < 2)
+                        return false
+                    if (this.user.i.length < 2)
+                        return false
+                    console.log("validateEmail", validateEmail(this.user.email))
+                    if (!validateEmail(this.user.email))
+                        return false
 
 
-
-        },
-        getSpk:function(id){
-            var ret=this.spk.filter(s=>s.id==id);
-            return ret[0];
-        }
-
-        ,
-        rotateTrack:function () {
-            return;
-            this.currShowTrack=this.tracks.shift();
-            this.tracks.push(this.currShowTrack);
-            this.trackRotateTimeout=setTimeout(()=>{this.rotateTrack()}, 10000)
-        },
-        previewTrack:function () {
-            var lastid=this.currShowTrack.id;
-            this.currShowTrack=this.tracks.pop();
-            this.tracks.unshift(this.currShowTrack);
-            if(lastid==this.currShowTrack.id)
-                return this.previewTrack();
-            clearTimeout(this.trackRotateTimeout)
-            this.trackRotateTimeout=setTimeout(()=>{this.rotateTrack()}, 10000)
-
-        },
-        nextTrack:function () {
-            var lastid=this.currShowTrack.id;
-            this.currShowTrack=this.tracks.shift();
-            this.tracks.push(this.currShowTrack);
-            if(lastid==this.currShowTrack.id)
-                return this.nextTrack();
-            clearTimeout(this.trackRotateTimeout)
-            this.trackRotateTimeout=setTimeout(()=>{this.rotateTrack()}, 10000)
-        },
-        changePgmItem:function(track){
-            this.currTrack=track;
-
-            EPPZScrollTo.scrollVerticalToElementById('pgmItems', 100)
-        },
-        showPopSpeaker:function (item) {
-            this.spkItemModal=item
-        }
-    },
-    watch:{
-        pgmItemModal:function () {
-            if(this.pgmItemModal)
-                document.body.style.overflow="hidden";
-            else
-                document.body.style.overflow="scroll";
-        },
-        spkItemModal:function () {
-            if(this.spkItemModal)
-                document.body.style.overflow="hidden";
-            else
-                document.body.style.overflow="scroll";
-        },
-        reqModal:function () {
-            if(this.reqModal) {
-                this.menuModal=false;
-                document.body.style.overflow = "hidden";
-            }
-            else
-                document.body.style.overflow="scroll";
-        },
-        menuModal:function () {
-            if(this.menuModal)
-                document.body.style.overflow="hidden";
-            else
-                document.body.style.overflow="scroll";
-        }
-    },
-    mounted:async function () {
-        moment.locale(lang)
-
-        this.tracks=(await axios.get('/api/tracks')).data;
-
-        this.tracks.forEach(t=>{
-            var key=moment(t.date).format("DD MMMM")
-            var items=this.tracksDate.filter(t=>{return t.key==key})
-            if(items.length==0) {
-                this.tracksDate.push({key: key, tracks:[]})
-            }
-            items=this.tracksDate.filter(t=>{return t.key==key})
-            t.key=key;
-            items[0].tracks.push(t);
-            items.sort(function(a, b){
-                if(a.key < b.key) { return -1; }
-                if(a.key > b.key) { return 1; }
-                return 0;
-            })
-
-        });
-        this.tracks.sort(function(a, b){
-            if(a.key < b.key) { return -1; }
-            if(a.key > b.key) { return 1; }
-            return 0;
-        });
-        this.rotateTrack();
-        this.currTrack=this.tracksDate[0].tracks[0];
-        var plenar=this.tracks.filter(t=>t.id==6);
-        if(plenar.length>0)
-            this.currShowTrack=plenar[0]
-        else
-            this.currShowTrack=this.tracks[0]
-        this.currTrack=this.currShowTrack
-        this.pgm=(await axios.get('/api/pgm')).data;
-
-        this.pgm.forEach(p=>{
-            var track=this.tracks.filter(t=>t.id==p.trackid);
-            if(track.length>0) {
-                p.key = track[0].key;
-
-            }
-
-        })
+                    return true;
 
 
-        this.spk=(await axios.get('/api/spk')).data;
-        this.spk.forEach(s=>{
-            if(s.i)
-            s.i=s.i.replace(/\s/g,"")
-            if(s.f)
-            s.f=s.f.replace(/\s/g,"")
-        })
-        var i=0;
-        var page=0;
-        this.curSpeakers[page]=[]
-           this.spk.forEach(s=>{
-               this.curSpeakers[page].push(s);
-               i++;
-               if(i>=8)
-               {
-                   i=0;
-                   page++;
-                   this.curSpeakers[page]=[]
-               }
-           })
-        this.faq=(await axios.get('/api/faq')).data;
-
-        var options = {
-            root: document.querySelector('#spkPage'),
-            rootMargin: '0px',
-            threshold: 0.1
-        }
-        var callback = (entries, observer) =>{
-            if(entries[0].isIntersecting){
-                var currLen=this.curSpeakers[this.curSpeakersPage].length
-                for(var i=currLen;i<this.spk.length && i<currLen+8  ;i++) {
-                    this.curSpeakers[this.curSpeakersPage].push(this.spk[i])
-                    console.log( this.curSpeakers[this.curSpeakersPage]);
+                },
+                getSpk: function (id) {
+                    var ret = this.spk.filter(s => s.id == id);
+                    return ret[0];
                 }
-                this.curSpeakers=this.curSpeakers.filter(r=>{return true})
+
+                ,
+                rotateTrack: function () {
+                    return;
+                    this.currShowTrack = this.tracks.shift();
+                    this.tracks.push(this.currShowTrack);
+                    this.trackRotateTimeout = setTimeout(() => {
+                        this.rotateTrack()
+                    }, 10000)
+                },
+                previewTrack: function () {
+                    var lastid = this.currShowTrack.id;
+                    this.currShowTrack = this.tracks.pop();
+                    this.tracks.unshift(this.currShowTrack);
+                    if (lastid == this.currShowTrack.id)
+                        return this.previewTrack();
+                    clearTimeout(this.trackRotateTimeout)
+                    this.trackRotateTimeout = setTimeout(() => {
+                        this.rotateTrack()
+                    }, 10000)
+
+                },
+                nextTrack: function () {
+                    var lastid = this.currShowTrack.id;
+                    this.currShowTrack = this.tracks.shift();
+                    this.tracks.push(this.currShowTrack);
+                    if (lastid == this.currShowTrack.id)
+                        return this.nextTrack();
+                    clearTimeout(this.trackRotateTimeout)
+                    this.trackRotateTimeout = setTimeout(() => {
+                        this.rotateTrack()
+                    }, 10000)
+                },
+                changePgmItem: function (track) {
+                    this.currTrack = track;
+
+                    EPPZScrollTo.scrollVerticalToElementById('pgmItems', 100)
+                },
+                showPopSpeaker: function (item) {
+                    this.spkItemModal = item
+                }
+            },
+            watch: {
+                pgmItemModal: function () {
+                    if (this.pgmItemModal)
+                        document.body.style.overflow = "hidden";
+                    else
+                        document.body.style.overflow = "scroll";
+                },
+                spkItemModal: function () {
+                    if (this.spkItemModal)
+                        document.body.style.overflow = "hidden";
+                    else
+                        document.body.style.overflow = "scroll";
+                },
+                reqModal: function () {
+                    if (this.reqModal) {
+                        this.menuModal = false;
+                        document.body.style.overflow = "hidden";
+                    } else
+                        document.body.style.overflow = "scroll";
+                },
+                menuModal: function () {
+                    if (this.menuModal)
+                        document.body.style.overflow = "hidden";
+                    else
+                        document.body.style.overflow = "scroll";
+                }
+            },
+            mounted: async function () {
+                moment.locale(lang)
+
+                this.tracks = (await axios.get('/api/tracks')).data;
+
+                this.tracks.forEach(t => {
+                    var key = moment(t.date).format("DD MMMM")
+                    var items = this.tracksDate.filter(t => {
+                        return t.key == key
+                    })
+                    if (items.length == 0) {
+                        this.tracksDate.push({key: key, tracks: []})
+                    }
+                    items = this.tracksDate.filter(t => {
+                        return t.key == key
+                    })
+                    t.key = key;
+                    items[0].tracks.push(t);
+                    items.sort(function (a, b) {
+                        if (a.key < b.key) {
+                            return -1;
+                        }
+                        if (a.key > b.key) {
+                            return 1;
+                        }
+                        return 0;
+                    })
+
+                });
+                this.tracks.sort(function (a, b) {
+                    if (a.key < b.key) {
+                        return -1;
+                    }
+                    if (a.key > b.key) {
+                        return 1;
+                    }
+                    return 0;
+                });
+                this.rotateTrack();
+                this.currTrack = this.tracksDate[0].tracks[0];
+                var plenar = this.tracks.filter(t => t.id == 6);
+                if (plenar.length > 0)
+                    this.currShowTrack = plenar[0]
+                else
+                    this.currShowTrack = this.tracks[0]
+                this.currTrack = this.currShowTrack
+                this.pgm = (await axios.get('/api/pgm')).data;
+
+                this.pgm.forEach(p => {
+                    var track = this.tracks.filter(t => t.id == p.trackid);
+                    if (track.length > 0) {
+                        p.key = track[0].key;
+
+                    }
+
+                })
+
+
+                this.spk = (await axios.get('/api/spk')).data;
+                this.spk.forEach(s => {
+                    if (s.i)
+                        s.i = s.i.replace(/\s/g, "")
+                    if (s.f)
+                        s.f = s.f.replace(/\s/g, "")
+                })
+                var i = 0;
+                var page = 0;
+                this.curSpeakers[page] = []
+                this.spk.forEach(s => {
+                    this.curSpeakers[page].push(s);
+                    i++;
+                    if (i >= 8) {
+                        i = 0;
+                        page++;
+                        this.curSpeakers[page] = []
+                    }
+                })
+                this.faq = (await axios.get('/api/faq')).data;
+
+                var options = {
+                    root: document.querySelector('#spkPage'),
+                    rootMargin: '0px',
+                    threshold: 0.1
+                }
+                var callback = (entries, observer) => {
+                    if (entries[0].isIntersecting) {
+                        var currLen = this.curSpeakers[this.curSpeakersPage].length
+                        for (var i = currLen; i < this.spk.length && i < currLen + 8; i++) {
+                            this.curSpeakers[this.curSpeakersPage].push(this.spk[i])
+                            console.log(this.curSpeakers[this.curSpeakersPage]);
+                        }
+                        this.curSpeakers = this.curSpeakers.filter(r => {
+                            return true
+                        })
+                    }
+                };
+                var observer = new IntersectionObserver(callback, options);
+                var target = document.querySelector('#spkChild');
+                setTimeout(() => {
+                    observer.observe(target);
+                    document.querySelectorAll('#spkChild').forEach(img => observer.observe(img))
+                }, 1000)
+
+
+                document.body.style.opacity = "1"
             }
-        };
-        var observer = new IntersectionObserver(callback, options);
-        var target = document.querySelector('#spkChild');
-        setTimeout(()=>{
-            observer.observe(target);
-            document.querySelectorAll('#spkChild').forEach(img => observer.observe(img))
-            },1000)
 
 
-        document.body.style.opacity="1"
-    }
+        })
 
+        window.addEventListener("scroll", setMenu)
+        setMenu();
 
-})
-window.addEventListener("scroll",setMenu)
-setMenu();
 function setMenu() {
     var a=window.scrollY;
     var elem=document.getElementById("headerMenuWr")
